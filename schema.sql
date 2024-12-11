263572456a48de8396b801e969dc769d2076a4d2
Uttam Karki <uttamkarkki18@gmail.com>
2024-12-11 10:32:31 +0545

    Added all files

--- schema.sql
+++ schema.sql
@@ -6,42 +6,80 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/I8hZjF
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-DROP TABLE IF EXISTS "confirmed";
-DROP TABLE IF EXISTS "recovered";
-DROP TABLE IF EXISTS "death";
+-- DROP TABLE IF EXISTS "confirmed";
+-- DROP TABLE IF EXISTS "recovered";
+-- DROP TABLE IF EXISTS "death";
 
-CREATE TABLE "confirmed" (
-    "country" VARCHAR   NOT NULL,
-    "date" DATE   NOT NULL,
-    "accumulated_confirmed" INT   NOT NULL,
-    "increase_of_confirmed" INT   NOT NULL
+-- CREATE TABLE "confirmed" (
+--     "country" VARCHAR   NOT NULL,
+--     "date" DATE   NOT NULL,
+--     "accumulated_confirmed" INT   NOT NULL,
+--     "increase_of_confirmed" INT   NOT NULL
+-- );
+
+-- CREATE TABLE "recovered" (
+--     "country" VARCHAR   NOT NULL,
+--     "date" DATE   NOT NULL,
+--     "accumulated_recovered" INT   NOT NULL,
+--     "increase_of_recovered" INT   NOT NULL
+-- );
+
+-- CREATE TABLE "death" (
+--     "country" VARCHAR   NOT NULL,
+--     "date" DATE   NOT NULL,
+--     "accumulated_deaths" INT   NOT NULL,
+--     "increase_of_deaths" INT   NOT NULL
+-- );
+
+-- -- Sanity checks post loading data from the jupyter notebook
+-- SELECT COUNT(*) from recovered;
+-- SELECT COUNT(*) FROM death;
+-- SELECT COUNT(*) FROM confirmed;
+
+
+
+-- -- This Doesn't work
+-- -- ALTER TABLE "recovered" ADD CONSTRAINT "fk_recovered_country_name_date" FOREIGN KEY("country_name", "date")
+-- -- REFERENCES "confirmed" ("country_name", "date");
+
+-- -- ALTER TABLE "death" ADD CONSTRAINT "fk_death_country_name_date" FOREIGN KEY("country_name", "date")
+-- -- REFERENCES "confirmed" ("country_name", "date");
+-- -- This Doesn't work
+
+
+DROP TABLE IF EXISTS `confirmed`;
+DROP TABLE IF EXISTS `recovered`;
+DROP TABLE IF EXISTS `death`;
+
+CREATE TABLE `confirmed` (
+    `country` VARCHAR(255) NOT NULL,
+    `date` DATE NOT NULL,
+    `accumulated_confirmed` INT NOT NULL,
+    `increase_of_confirmed` INT NOT NULL,
+    PRIMARY KEY (`country`, `date`)
 );
 
-CREATE TABLE "recovered" (
-    "country" VARCHAR   NOT NULL,
-    "date" DATE   NOT NULL,
-    "accumulated_recovered" INT   NOT NULL,
-    "increase_of_recovered" INT   NOT NULL
+CREATE TABLE `recovered` (
+    `country` VARCHAR(255) NOT NULL,
+    `date` DATE NOT NULL,
+    `accumulated_recovered` INT NOT NULL,
+    `increase_of_recovered` INT NOT NULL,
+    PRIMARY KEY (`country`, `date`),
+    CONSTRAINT `fk_recovered_country_date` FOREIGN KEY (`country`, `date`)
+        REFERENCES `confirmed` (`country`, `date`)
 );
 
-CREATE TABLE "death" (
-    "country" VARCHAR   NOT NULL,
-    "date" DATE   NOT NULL,
-    "accumulated_deaths" INT   NOT NULL,
-    "increase_of_deaths" INT   NOT NULL
+CREATE TABLE `death` (
+    `country` VARCHAR(255) NOT NULL,
+    `date` DATE NOT NULL,
+    `accumulated_deaths` INT NOT NULL,
+    `increase_of_deaths` INT NOT NULL,
+    PRIMARY KEY (`country`, `date`),
+    CONSTRAINT `fk_death_country_date` FOREIGN KEY (`country`, `date`)
+        REFERENCES `confirmed` (`country`, `date`)
 );
 
--- Sanity checks post loading data from the jupyter notebook
-SELECT COUNT(*) from recovered;
-SELECT COUNT(*) FROM death;
-SELECT COUNT(*) FROM confirmed;
-
-
-
--- This Doesn't work
--- ALTER TABLE "recovered" ADD CONSTRAINT "fk_recovered_country_name_date" FOREIGN KEY("country_name", "date")
--- REFERENCES "confirmed" ("country_name", "date");
-
--- ALTER TABLE "death" ADD CONSTRAINT "fk_death_country_name_date" FOREIGN KEY("country_name", "date")
--- REFERENCES "confirmed" ("country_name", "date");
--- This Doesn't work
+-- Sanity checks post loading data from the Jupyter notebook
+SELECT COUNT(*) FROM `recovered`;
+SELECT COUNT(*) FROM `death`;
+SELECT COUNT(*) FROM `confirmed`;
